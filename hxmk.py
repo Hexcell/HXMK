69a72c6d2385002ce072b22bb063d8a5a52d81d4
Fabian <fabian0010k@gmail.com>
2019-03-11 18:36:43 +0100

    Updated buildscript to enable building and distributing

--- hxmk.py
+++ hxmk.py
@@ -1,3 +1,11 @@
 @rule()
 def everything(c):
 	c << "pip install ."
+
+@rule()
+def upload(c) -> "build":
+	c << "twine upload dist/*"
+
+@rule()
+def build(c):
+	c << "python setup.py sdist bdist_wheel"
